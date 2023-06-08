4125c34fb2fc4d2561c69f640389066a2c5d90e8
Jonatas <stylelife.js@gmail.com>
2023-06-08 09:34:22 -0300

    Add files via upload

---
+++ script/carregarStyle.js
@@ -0,0 +1,9 @@
+function carregarStyle(){
+    document.querySelector('body').style = 'background-image: linear-gradient(180deg, #024467, #036fa9); height: 100vh;';
+    document.querySelector('#container').style = 'background-color: rgba(0,0,0, .0); display: block;';
+    document.querySelector('.titulo').style = 'color: #ffffff;  margin: 10px 0 0 0;';
+    document.querySelector('.pontuacao').style = 'background-color: rgba(0,0,0, .0); justify-content: flex-start; padding: 0px 0px 20px 0px;';
+    document.querySelector('#pontuacao').style = 'margin-left: 25px; margin-top: 0; align-items: flex-start;';
+    document.querySelector('.barra-data').style = 'background-color: rgba(0,0,0, .0);';
+    document.querySelector('.barra-logo').style = 'background-color: rgba(0,0,0, .0);';
+}
---
+++ script/perguntas.js
@@ -0,0 +1,300 @@
+
+
+function funcao_questoes(){
+    return  [`
+<div class="titulo-pergunta A1cbe0"><span class="gerar-n-questao"></span>)
+    Escolha o elemento HTML correto para o título maior:
+</div>
+
+<ul>
+    <li class="hover">
+
+        A)&lth1&gt &lth1/&gt</li>
+    <li class="hover">
+
+        B) font-size: 16px;</li>
+    <li class="hover">
+
+        C) &lth4&gt &lth4/&gt</li>
+    <li class="hover">
+
+        D) &ltdiv&gt &ltdiv/&gt</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A5abb3"><span class="gerar-n-questao"></span>) Qual é o elemento HTML correto para inserir uma quebra de linha?
+</div>
+<ul>
+    <li class="hover">
+
+        A) \\n </li>
+    <li class="hover">
+
+        B) &ltspan&gt &ltspan/&gt</li>
+    <li class="hover">
+
+        C) &ltp&gt</li>
+    <li class="hover">
+
+        D) &ltbr&gt</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+</div>`, `
+<div class="titulo-pergunta A7cbe0"><span class="gerar-n-questao"></span>) Qual é o HTML correto para criar um hiperlink?</div>
+
+<ul>
+    <li class="hover">
+
+        A) &lta href="http://..."&gt &lta/&gt</li>
+    <li class="hover">
+
+        B) &lta http://...&gt &lta/&gt</li>
+    <li class="hover">
+
+        C) &lta link="http://..."&gt &lta/&gt</li>
+    <li class="hover">
+
+        D) &lta url="http://..."&gt &lta/&gt</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A9cfe2"><span class="gerar-n-questao"></span>) Qual é a sintaxe CSS correta?</div>
+
+<ul>
+    <li class="hover">
+
+        A) body {color = rgb(0,0,0)}</li>
+    <li class="hover">
+
+        B) {body: color:#000;}</li>
+    <li class="hover">
+
+        C) body {color: black;}</li>
+    <li class="hover">
+
+        D) body:color=black;</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A4bca1"><span class="gerar-n-questao"></span>) Como você insere um comentário em um arquivo CSS?</div>
+
+<ul>
+    <li class="hover">
+
+        A) //comentário </li>
+    <li class="hover">
+
+        B) /*comentário*/</li>
+    <li class="hover">
+
+        C) &lta!--comentário--&gt</li>
+    <li class="hover">
+
+        D) /*comentário//</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A3cbf0"><span class="gerar-n-questao"></span>) Qual propriedade no CSS é usada para alterar a cor de fundo?</div>
+
+<ul>
+    <li class="hover">
+
+        A) background-color</li>
+    <li class="hover">
+
+        B) bgcolor</li>
+    <li class="hover">
+
+        C) color</li>
+    <li class="hover">
+
+        D) bg-color</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A7ffe2"><span class="gerar-n-questao"></span>) Dentro de qual elemento HTML colocamos o JavaScript?</div>
+
+<ul>
+    <li class="hover">
+
+        A) &ltscript href=" "&gt</li>
+    <li class="hover">
+
+        B) &ltjs&gt</li>
+    <li class="hover">
+
+        C) &ltscript&gt
+    </li>
+    <li class="hover">
+
+        D) &lttypescript&gt
+    </li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A9aee3"><span class="gerar-n-questao"></span>) Qual é a sintaxe JavaScript correta para alterar o conteúdo do
+    elemento HTML abaixo?<br>
+    <code>var texto = document.querySelector('#texto')</code>
+
+</div>
+
+<ul>
+    <li class="hover">
+
+        A) innerHTML = Novo Texto</li>
+    <li class="hover">
+
+        B) texto.insertText(Novo Texto)</li>
+    <li class="hover">
+
+        C) texto.newText = 'Novo Texto'</li>
+    <li class="hover">
+
+        D) texto.innerText = 'Novo Texto'</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A0cfe3"><span class="gerar-n-questao"></span>) Uma das maneiras de criar funções em JavaScript é: </div>
+
+<ul>
+    <li class="hover">
+
+        A) new function()={}</li>
+    <li class="hover">
+
+        B) texto(){}</li>
+    <li class="hover">
+
+        C) function texto() = {}</li>
+    <li class="hover">
+
+        D) const Texto = () =>{}</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `<div class="titulo-pergunta A1cbe2"><span class="gerar-n-questao"></span>) Como declarar variáveis em JavaScript?</div>
+
+<ul>
+    <li class="hover">
+
+        A) var, char, int</li>
+    <li class="hover">
+
+        B) var, let, int</li>
+    <li class="hover">
+
+        C) const, var, let</li>
+    <li class="hover">
+
+        D) char, int, float</li>
+    <li class="hover">
+
+        E) number, string, boolean</li>
+</ul>`
+,
+`<div class="titulo-pergunta A1cbe2"><span class="gerar-n-questao"></span>) O que significa CSS?</div>
+
+<ul>
+    <li class="hover">
+
+        A) Computer Style Sheets</li>
+    <li class="hover">
+
+        B) Creative Style Sheets</li>
+    <li class="hover">
+
+        C) Cascading Style Sheets</li>
+    <li class="hover">
+
+        D) Colorful Style Sheets</li>
+    <li class="hover">
+
+        E) Cascading Computer Sheets</li>
+</ul>`
+, `
+<div class="titulo-pergunta A0cfe3"><span class="gerar-n-questao"></span>) Para exibir valores no corpo da página, é preciso usar a função </div>
+
+<ul>
+    <li class="hover">
+
+        A) print()</li>
+    <li class="hover">
+
+        B) alert()</li>
+    <li class="hover">
+
+        C) Windows.open()</li>
+    <li class="hover">
+
+        D) document.write()</li> 
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`, `
+<div class="titulo-pergunta A3cbf0"><span class="gerar-n-questao"></span>) Qual atributo HTML é usado para definir estilos embutidos?</div>
+
+<ul>
+    <li class="hover">
+
+        A) style</li>
+    <li class="hover">
+
+        B) class</li>
+    <li class="hover">
+
+        C) font</li>
+    <li class="hover">
+
+        D) styles</li>
+    <li class="hover">
+
+        E) color</li>
+</ul>
+`, `
+<div class="titulo-pergunta A9aee3"><span class="gerar-n-questao"></span>) Como você exibe hiperlinks sem sublinhado?
+    
+
+</div>
+
+<ul>
+    <li class="hover">
+
+        A)  a {text-decoration:no-underline;}</li>
+    <li class="hover">
+
+        B)  a {underline:none;}</li>
+    <li class="hover">
+
+        C)  a {decoration:no-underline;}</li>
+    <li class="hover">
+
+        D)  a {text-decoration:none;}</li>
+    <li class="hover">
+
+        E) N.D.A</li>
+</ul>
+`,
+
+
+]
+
+
+}
