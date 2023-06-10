82b6ad4ce2f797d166ceabd5a2574589ed1430d6
Jonatas <stylelife.js@gmail.com>
2023-06-10 14:38:10 -0300

    Update carregarStyle.js

--- script/carregarStyle.js
+++ script/carregarStyle.js
@@ -1,5 +1,5 @@
 function carregarStyle(){
-    document.querySelector('body').style = 'background-image: linear-gradient(180deg, #024467, #036fa9); height: 100vh;';
+    document.querySelector('body').style = 'background-image: linear-gradient(180deg, #024467, #036fa9); min-height: 100vh;';
     document.querySelector('#container').style = 'background-color: rgba(0,0,0, .0); display: block;';
     document.querySelector('.titulo').style = 'color: #ffffff;  margin: 10px 0 0 0;';
     document.querySelector('.pontuacao').style = 'background-color: rgba(0,0,0, .0); justify-content: flex-start; padding: 0px 0px 20px 0px;';
